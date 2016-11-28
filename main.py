66fe9745ce4dd128808c6851b561a863ac4678b8
greysome <wayyan.win@gmail.com>
2016-11-29 00:53:00 +0800

    Border drawing is done only if the current state is STATE_GAME

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import curses
-import sys
 
 def parse(file):
     def get_config_value(str):
@@ -26,25 +25,10 @@ config = parse(f)
 screen = curses.initscr()
 dim = screen.getmaxyx()
 
-if config['size'] != 'FULLSCREEN':
-    x_offset = config['size'][1]/2
-    y_offset = config['size'][0]/2
-
-    x_center = dim[1]/2
-    y_center = dim[0]/2
-
-    # Draw borders
-    screen.addstr(y_center+(y_offset+1), x_center-(x_offset-1),
-                  "_"*(config['size'][1]-1))
-    screen.addstr(y_center-(y_offset+1), x_center-(x_offset-1),
-                  "_"*(config['size'][1]-1))
-    for i in range(y_center-(y_offset-1), y_center+(y_offset+1)):
-        screen.addstr(i, x_center-x_offset, "|")
-        screen.addstr(i, x_center+x_offset, "|")
-    
-screen.refresh()
-screen.getch()
-curses.endwin()
+x_offset = config['size'][1]/2
+y_offset = config['size'][0]/2
+x_center = dim[1]/2
+y_center = dim[0]/2
 
 STATE_MENU = 0
 STATE_GAME = 1
@@ -53,7 +37,20 @@ current_state = STATE_MENU
 # Main game loop
 while True:
     if current_state == STATE_MENU:
-        pass
+        screen.addstr(y_center+(y_offset+3), x_center-(x_offset),
+                      "slithery")
 
-    elif current_state == STATE_GAME:
-        pass
+    elif current_state == STATE_GAME: 
+        if config['size'] != 'FULLSCREEN':
+            # Draw borders
+            screen.addstr(y_center+(y_offset+1), x_center-(x_offset-1),
+                          "_"*(config['size'][1]-1))
+            screen.addstr(y_center-(y_offset+1), x_center-(x_offset-1),
+                          "_"*(config['size'][1]-1))
+            for i in range(y_center-(y_offset-1), y_center+(y_offset+1)):
+                screen.addstr(i, x_center-x_offset, "|")
+                screen.addstr(i, x_center+x_offset, "|")
+            
+        screen.refresh()
+        screen.getch()
+        curses.endwin()
