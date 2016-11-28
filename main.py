a615d05d68f7a2f8d367dedfb537db3147d363e3
greysome <wayyan.win@gmail.com>
2016-11-28 23:43:28 +0800

    Only draw borders if size is not fullscreen

--- main.py
+++ main.py
@@ -25,20 +25,22 @@ def parse(file):
 f = open("config", 'rb')
 config = parse(f)
 
-x_offset = config['size'][1]/2
-y_offset = config['size'][0]/2
+if config['size'] != 'FULLSCREEN':
+    x_offset = config['size'][1]/2
+    y_offset = config['size'][0]/2
 
-x_center = dim[1]/2
-y_center = dim[0]/2
+    x_center = dim[1]/2
+    y_center = dim[0]/2
 
-# Draw borders
-screen.addstr(y_center+(y_offset+1), x_center-(x_offset-1),
-              "_________")
-screen.addstr(y_center-(y_offset+1), x_center-(x_offset-1),
-              "_________")
-for i in range(y_center-(y_offset-1), y_center+(y_offset+1)):
-    screen.addstr(i, x_center-x_offset, "|")
-    screen.addstr(i, x_center+x_offset, "|")
+    # Draw borders
+    screen.addstr(y_center+(y_offset+1), x_center-(x_offset-1),
+                  "_________")
+    screen.addstr(y_center-(y_offset+1), x_center-(x_offset-1),
+                  "_________")
+    for i in range(y_center-(y_offset-1), y_center+(y_offset+1)):
+        screen.addstr(i, x_center-x_offset, "|")
+        screen.addstr(i, x_center+x_offset, "|")
+    
 screen.refresh()
 screen.getch()
 curses.endwin()
