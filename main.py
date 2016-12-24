c684bf22f48c07baabe034e7f563b604a2a8b7b9
wayyan1000 <wayyan.win@gmail.com>
2016-12-25 00:12:48 +0800

    Modified code formatting to conform to pylint standards

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import sys
 import values
 
 
-class BaseItem:
+class BaseItem(object):
     def __init__(self):
         pass
 
-    
+
     def get_spawn(self, board):
         while True:
             # Spawn at least a quarter into the board
-            spawn_coords = (random.randint((1/4)*values.HEIGHT, (3/4)*values.HEIGHT), 
+            spawn_coords = (random.randint((1/4)*values.HEIGHT, (3/4)*values.HEIGHT),
                             random.randint((1/4)*values.LENGTH, (3/4)*values.LENGTH))
 
             # Ensure there are no other powerups occupying the same spot
@@ -25,26 +25,19 @@ class BaseItem:
 class Snake(BaseItem):
     coords = []
 
-    def __init__(self, board):
-        pass
+    def __init__(self):
+        super(Snake, self).__init__()
 
 
     def get_spawn(self, board):
         spawn_coords = super(Snake, self).get_spawn(board)
-        coords.append(spawn_coords)
+        self.coords.append(spawn_coords)
         return spawn_coords
-
-
-    def length(self):
-        length = 0
-        for key, value in coords.iteritems():
-            length += len(value)
-        return length
 
 
     def extrapolate(self, direction):
         extrapolation_map = {values.DIRECTION_UP: (-1, 0),
-                             values.DIRECTON_DOWN: (1, 0),
+                             values.DIRECTION_DOWN: (1, 0),
                              values.DIRECTION_LEFT: (0, -1),
                              values.DIRECTION_RIGHT: (0, 1)}
         return (self.coords[0][i] + extrapolation_map[direction][i] for i in range(2))
@@ -63,10 +56,10 @@ class Snake(BaseItem):
         self.coords.append(spawn_map[direction])
 
 
-class Board:
+class Board(object):
     grid = []
 
-    def __init__(self, screen, config):
+    def __init__(self, screen):
         self.screen = screen
 
         for i in range(values.HEIGHT):
@@ -75,11 +68,16 @@ class Board:
                 row.append('')
             self.grid.append(row)
 
-    
-    def at(self, coords):
-        return grid[coords[0]][coords[1]]
 
-    
+    def at(self, coords):
+        return self.grid[coords[0]][coords[1]]
+
+
+    def within(self, coords):
+        return coords[0] < 0 or coords[0] >= values.HEIGHT \
+            or coords[1] < 0 or coords[1] >= values.LENGTH
+
+
     def draw(self):
         for index, row in enumerate(self.grid):
             self.screen.addstr(values.CORNERS['top_left'][0]+index, values.CORNERS['top_left'][1],
@@ -88,12 +86,12 @@ class Board:
 
     def apply(self, items):
         def replace(item, coords):
-            board[coords[0]][coords[1]] = item
+            self.grid[coords[0]][coords[1]] = item
 
         item_map = {values.ITEM_FOOD: config['snake'],
                     values.ITEM_SNAKE: config['food']}
 
-        __clear()
+        self.__clear()
         for key, value in items.iteritems():
             for i in value:
                 replace(item_map[key], i)
@@ -102,62 +100,39 @@ class Board:
     def __clear(self):
         for i in range(values.HEIGHT):
             for j in range(values.LENGTH):
-                board[i][j] = ''
+                self.grid[i][j] = ''
 
 
-class Game:
+class Game(object):
     current_direction = None
 
 
     def __init__(self, config, screen):
         self.config = config
         self.screen = screen
+        self.board = None
 
 
-    def start():
-        self.board = Board(self.screen, self.config)
+    def start(self):
+        self.board = Board(self.screen)
 
 
-    def draw_borders():
-        self.screen.addstr(values.CORNERS['top_left'][0]-1, values.CORNERS['top_left'][1], values.CORNERS['top']*x)
-        self.screen.addstr(values.CORNERS['bottom_left'][0], values.CORNERS['bottom_left'][1], values.CORNERS['bottom']*x)
+    def draw_borders(self):
+        self.screen.addstr(values.CORNERS['top_left'][0]-1, values.CORNERS['top_left'][1],
+                           values.CORNERS['top']*values.LENGTH)
+        self.screen.addstr(values.CORNERS['bottom_left'][0], values.CORNERS['bottom_left'][1],
+                           values.CORNERS['bottom']*values.LENGTH)
+
         for i in range(values.CORNERS['top_left'][0], values.CORNERS['bottom_left'][0]):
-            self.screen.addstr(i, values.CORNERS['top_left'][1]-1, values.CORNERS['left']) 
+            self.screen.addstr(i, values.CORNERS['top_left'][1]-1, values.CORNERS['left'])
             self.screen.addstr(i, values.CORNERS['top_right'][1]+1, values.CORNERS['right'])
-
-
-    def iteration(char):
-        direction_keymap = {'w': DIRECTION_UP,
-                            's': DIRECTION_DOWN,
-                            'a': DIRECTION_left,
-                            'd': DIRECTION_right}
-
-        # The x and y values to move the snake's head
-        coord_diff = {DIRECTION_UP: (-1, 0),
-                             DIRECTION_DOWN: (1, 0),
-                             DIRECTION_left: (0, -1),
-                             DIRECTION_right: (0, 1)}
-
-        char_lower = str.lower(char)
-        current_direction = direction_keymap[char]
-        new_head = coord_add(snake_head, coord_diff[current_direction])
-
-        # Out of borders
-        if new_head[0] < self.corners['top_left'][0] or new_head[0] > corners['bottom_left'][0] \
-                or new_head[1] < self.corners['top_left'][1] or new_head[0] > corners['top_right'][1]:
-            return 1
-        else:
-            snake_head = new_head
-
-            if snake_head == 0:
-                pass
 
 
 if __name__ == '__main__':
     screen = curses.initscr()
     curses.curs_set(0)
     curses.noecho()
-    screen_dimensions = screen.getmaxyx()
+    values.SCREEN_DIMENSIONS = screen.getmaxyx()
 
 
     def terminate(*args):
@@ -180,19 +155,19 @@ if __name__ == '__main__':
                 size = get_config_value(line)
 
                 if size == 'FULLSCREEN':
-                    value.update({'size': dim})
+                    value.update({'size': values.SCREEN_DIMENSIONS})
                 else:
                     size_split = tuple(int(size.split('x')[i]) for i in range(2))
 
-                    if any(size_split[i] > dimensions[i] for i in range(2)):
-                        terminate("Error: width and height must not be greater than the " +
+                    if any(size_split[i] > values.SCREEN_DIMENSIONS[i] for i in range(2)):
+                        terminate("Error: width and HEIGHT must not be greater than the " +
                                   "maximum dimensions")
 
                     if any(size_split[i] < 10 for i in range(2)):
-                        terminate("Error: width and height must be both at least 10")
+                        terminate("Error: width and HEIGHT must be both at least 10")
 
                     if any(size_split[i]%2 != 0 for i in range(2)):
-                        terminate("Error: width and height must be even numbers")
+                        terminate("Error: width and HEIGHT must be even numbers")
 
                     value.update({'size': size_split})
 
@@ -204,7 +179,7 @@ if __name__ == '__main__':
                     terminate("Error: each border must be only 1 character")
 
                 # Left, right, top, bottom
-                value.update({'borders': borders_split}) 
+                value.update({'borders': borders_split})
 
             if 'snake' in line:
                 snake_char = get_config_value(line)
@@ -212,15 +187,15 @@ if __name__ == '__main__':
                 if len(snake_char) != 1:
                     terminate("Error: snake must be only 1 character")
 
-                values.update({'snake': snake_char})
+                value.update({'snake': snake_char})
 
             if 'food' in line:
                 food_char = get_config_value(line)
-                
+
                 if len(food_char) != 1:
                     terminate("Error: food must be only 1 character")
 
-                values.update({'food': food_char})
+                value.update({'food': food_char})
 
         return value
 
@@ -236,21 +211,24 @@ if __name__ == '__main__':
         config = parse(config_file)
 
         # curses coordinates are in the format (y, x)
-        values.SCREEN_DIMENSIONS = screen_dimensions
         values.LENGTH = config['size'][1]
         values.HEIGHT = config['size'][0]
-        values.X_CENTER = dimensions[1]/2
-        values.Y_CENTER = dimensions[0]/2
+        values.X_CENTER = values.SCREEN_DIMENSIONS[1]/2
+        values.Y_CENTER = values.SCREEN_DIMENSIONS[0]/2
 
         values.BORDERS = {'top': config['borders'][2],
                           'bottom': config['borders'][3],
                           'left': config['borders'][0],
                           'right': config['borders'][1]}
 
-        values.CORNERS = {'top_left': (values.y_center-values.height/2+2, values.x_center-values.length/2+1),
-                          'top_right': (values.y_center-values.height/2+2, values.x_center-values.length/2),
-                          'bottom_left': (values.y_center+values.height/2+2, values.x_center-values.length/2+1),
-                          'bottom_right': (values.y_center+values.height/2+2, values.x_center+values.length/2)}
+        values.CORNERS = {'top_left': (values.Y_CENTER-values.HEIGHT/2+2,
+                                       values.X_CENTER-values.LENGTH/2+1),
+                          'top_right': (values.Y_CENTER-values.HEIGHT/2+2,
+                                        values.X_CENTER-values.LENGTH/2),
+                          'bottom_left': (values.Y_CENTER+values.HEIGHT/2+2,
+                                          values.X_CENTER-values.LENGTH/2+1),
+                          'bottom_right': (values.Y_CENTER+values.HEIGHT/2+2,
+                                           values.X_CENTER+values.LENGTH/2)}
 
 
     # Main game loop
